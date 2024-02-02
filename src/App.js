b8a6497b47c289770099d8dfa112476d24d0e465
hasanBafuleh <hasanbafuleh@gmail.com>
2024-02-02 19:55:18 +0300

    Adding the css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com?apikey=ebefa568";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.search);
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return <h1>App</h1>;
 };
 
