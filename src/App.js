e265959e67968886bc5e0a934c44ed3457258d79
hasanBafuleh <hasanbafuleh@gmail.com>
2024-04-24 01:21:17 +0300

    http to https

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=ebefa568";
+const API_URL = "https://www.omdbapi.com?apikey=ebefa568";
+
 
 const App = () => {
   const [movies, setMovies] = useState([]);
